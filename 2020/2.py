b4c8b2fe6ace2fd37a8102dc875b6828a8638980
Anthea <antheargh@gmail.com>
2020-12-11 16:30:11 +1100

    Day 2 Pt 2

--- 2020/2.py
+++ 2020/2.py
@@ -14,12 +14,11 @@ def main(inputf):
             lim = re.search("(\d+)\-(\d+)", entries[0]).group(1, 2)
             char = entries[1][0]
             data.append([lim, char, entries[2]])
-    print(data)
 
     ans_1 = pt_1(deepcopy(data))
     print(ans_1)
 
-    ans_2 = pt_2()
+    ans_2 = pt_2(deepcopy(data))
     print(ans_2)
 
 
@@ -36,8 +35,17 @@ def pt_1(data):
 
 
 
-def pt_2():
-    pass
+def pt_2(data):
+    valid = []
+    for d in data:
+        p1, p2 = int(d[0][0]), int(d[0][1])
+        char = d[1]
+        pw = d[2]
+        if (pw[p1-1] == char and pw[p2-1] != char) or (pw[p2-1] == char and pw[p1-1] != char):
+            valid.append(pw)
+
+    return len(valid)
+
 
 
 
