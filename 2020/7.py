5f602115ba726b14b7ce2832a229e0a736cc44a4
Anthea <antheargh@gmail.com>
2020-12-15 14:41:02 +1100

    Day 7 Pt 2)

--- 2020/7.py
+++ 2020/7.py
@@ -3,6 +3,7 @@
 import sys
 from copy import deepcopy
 import re
+import numpy as np
 
 
 # Run with input file name/path as single argument
@@ -13,12 +14,11 @@ def main(inputf):
         for line in f:
             rules.append(line.strip())
 
-    print(rules)
 
     ans_1 = pt_1(deepcopy(rules), "shiny gold")
     print(ans_1)
 
-    ans_2 = pt_2()
+    ans_2 = pt_2(deepcopy(rules), "shiny gold")
     print(ans_2)
 
 
@@ -28,10 +28,9 @@ def pt_1(rules, bag_c):
     for rule in rules:
         colours.append(re.search("(.+) bags ", rule).group(1))
 
-    # Dict of what bags can contain that coloured bag
+    # Dict of bags that the coloured bag is contained by
     bag_dict = dict()
     for colour in colours:
-        print(colour)
         can_contain = []
         for rule in rules:
             if re.search("\d "+colour, rule):
@@ -46,13 +45,35 @@ def pt_1(rules, bag_c):
         parent_bags.append(next_bag)
         if len(bag_dict[next_bag]):
             unchecked.extend(bag_dict[next_bag])
-    return(len(set(parent_bags)))
+    return len(set(parent_bags))
 
 
 
-def pt_2():
-    pass
+def pt_2(rules, bag_c):
+    bag_dict = dict()
 
+    # Dict of what each coloured bag can contain
+    for rule in rules:
+        bag = re.search("(.+) bags contain", rule).group(1)
+        contains = []
+        contained_bags = re.findall("(\d+) (\D+ \D+) bag", rule)
+        for b in contained_bags:
+            n, c = int(b[0]), b[1]
+            contains.append((n, c))
+        bag_dict[bag] = contains
+
+    # Use recursion to find total number of bags
+
+    n = calc_total(bag_c, bag_dict)
+
+    return n
+
+def calc_total(bag, bag_dict):
+    total = 0
+    if bag_dict[bag]:
+        for (n, b) in bag_dict[bag]:
+            total += n + n*calc_total(b, bag_dict)
+    return total
 
 
 
