090978d3f5e76175253122f55e0327b58ca55d57
Anthea <antheargh@gmail.com>
2020-12-18 02:13:53 +1100

    Day 13 Pt 2

--- 2020/13.py
+++ 2020/13.py
@@ -2,7 +2,9 @@
 
 import sys
 from copy import deepcopy
+import numpy as np
 
+MIN_TIME = 100000000000000
 
 # Run with input file name/path as single argument
 
@@ -16,10 +18,10 @@ def main(inputf):
     buses = data[1].split(',')
 
 
-    ans_1 = pt_1(arrival, buses)
+    ans_1 = pt_1(arrival, deepcopy(buses))
     print(ans_1)
 
-    ans_2 = pt_2()
+    ans_2 = pt_2(deepcopy(buses))
     print(ans_2)
 
 
@@ -37,8 +39,23 @@ def pt_1(arrival, buses):
 
 
 
-def pt_2():
-    pass
+def pt_2(buses):
+    bus_dict = {}
+    for n, b in enumerate(buses):
+        if b == "x":
+            pass
+        else:
+            bus_dict[int(b)] = (int(b) - n) % int(b)
+    print(bus_dict)
+    time = 0
+    increment = 1
+    for b, n in bus_dict.items():
+        while time % b != n:
+            time += increment
+        increment *= b
+
+    return time
+
 
 
 
@@ -46,4 +63,5 @@ def pt_2():
 if __name__ == '__main__':
     inputf = sys.argv[-1]
     main(inputf)
+
 
