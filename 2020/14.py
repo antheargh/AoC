77009fd1832c8be1ac08f7ebb05b36ef6d69b8ec
Anthea <antheargh@gmail.com>
2020-12-21 14:33:27 +1100

    Day 14 pt 2

--- 2020/14.py
+++ 2020/14.py
@@ -3,6 +3,7 @@
 import sys
 from copy import deepcopy
 import re
+import itertools
 
 
 # Run with input file name/path as single argument
@@ -20,7 +21,7 @@ def main(inputf):
     ans_1 = pt_1(deepcopy(data))
     print(ans_1)
 
-    ans_2 = pt_2()
+    ans_2 = pt_2(deepcopy(data))
     print(ans_2)
 
 
@@ -45,9 +46,25 @@ def pt_1(data):
     return sum
 
 
-def pt_2():
-    pass
+def pt_2(data):
+    mem = {}
+    for d in data:
+        if d[0] == "mask":
+            mask_1 = [n for n, b in enumerate(d[1]) if b == "1"]
+            mask_x = [n for n, b in enumerate(d[1]) if b == "X"]
+        else:
+            loc = int(re.search("(\d+)", d[0]).group(0))
+            loc_bin = [b for b in "{:036b}".format(loc)]
+            floats = list(itertools.product([0, 1], repeat=len(mask_x)))
+            for i in mask_1:
+                loc_bin[i] = "1"
+            for f in floats:
+                curr_loc = loc_bin
+                for i, x in enumerate(mask_x):
+                    curr_loc[x] = str(f[i])
+                mem[''.join(curr_loc)] = int(d[1])
 
+    return sum(mem.values())
 
 
 
